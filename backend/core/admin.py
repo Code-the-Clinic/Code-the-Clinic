502f216bc3e68372f4a8696eff0df71499e0d56b
hrhendersonboyer-admin <hrhendersonboyer@crimson.ua.edu>
2026-02-19 15:00:36 -0600

    Revert "Merge branch 'main' of https://github.com/Code-the-Clinic/Code-the-Clinic"
    This reverts commit 2654198f42f362df79a5a4eda79112ae7a18ecdd, reversing
    changes made to 6fc2db921949aa52dbbaf0c344ecce3cb890b88e.

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,11 +1,3 @@
 from django.contrib import admin
-from .models import UserActivity
-
-
-@admin.register(UserActivity)
-class UserActivityAdmin(admin.ModelAdmin):
-    list_display = ("user", "action", "ip_address", "created_at")
-    list_filter = ("user",)
-    search_fields = ("action", "user__username", "ip_address")
 
 # Register your models here.
