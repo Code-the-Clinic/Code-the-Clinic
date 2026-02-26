dd5e8d3d3b72a39e1ced62d6f81b601e446335f0
hrhendersonboyer-admin <hrhendersonboyer@crimson.ua.edu>
2026-02-26 16:46:23 -0600

    [Security] Remove last instance of @csrf_exempt to properly use csrf tokens on all endpoints

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
-from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
 from django.db.models import Avg, Sum, F, Case, When, IntegerField, Value, FloatField
 from django.db.models.functions import Coalesce
